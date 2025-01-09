7e889aa27e51a2dfa4a7d3e26526b762753b4e5e
Phiko <phgu03@gmail.com>
2025-01-09 13:15:16 +0100

    ✨ feat(schemas): Enhance alert schemas with validation for update operations

--- backend/src/schemas/alert.ts
+++ backend/src/schemas/alert.ts
@@ -1,19 +1,34 @@
 import Joi from "joi";
 import { UuidSchema } from "./general";
 
+const name = Joi.string().min(3).max(255);
+const description = Joi.string().min(3).max(255);
+const type = Joi.string().lowercase().trim().valid('info', 'warning', 'error');
+const identifier = Joi.string().lowercase().trim().valid("celsius","fahrenheit","percentage");
+
 export const getAlertSchema = Joi.object({
     id: UuidSchema,
-    name: Joi.string().min(3).max(255),
-    type: Joi.string().lowercase().trim().valid('info', 'warning', 'error'),
-    Identifier: Joi.string().lowercase().trim().valid("celsius","fahrenheit","percentage"),
+    name,
+    type,
+    identifier,
     deviceId: UuidSchema,
 }).rename('id', 'uuid', { alias: true, ignoreUndefined: true });
 
 export const createAlertSchema = Joi.object({
-    name: Joi.string().min(3).max(255).required(),
-    description: Joi.string().min(3).max(255).required(),
+    name: name.required(),
+    description: description.required(),
     threshold: Joi.number().required(),
-    type: Joi.string().lowercase().trim().valid('info', 'warning', 'error').required(),
-    Identifier: Joi.string().lowercase().trim().valid("celsius","fahrenheit","percentage").required(),
+    type: type.required(),
+    identifier: identifier.required(),
     deviceId: UuidSchema.required(),
 });
+
+export const updateAlertSchema = Joi.array().items(Joi.object({
+    uuid: UuidSchema.required(),
+    name,
+    description,
+    threshold: Joi.number(),
+    type,
+    identifier,
+    deviceId: UuidSchema,
+}));
