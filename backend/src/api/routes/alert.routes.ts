72322422326ba84e78e07649721442169fb03f90
Phiko <phgu03@gmail.com>
2025-01-09 13:12:48 +0100

    ✨ feat(routes): Add PATCH route for updating alerts

--- backend/src/api/routes/alert.routes.ts
+++ backend/src/api/routes/alert.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 
-import { createRecord, getAll } from '@controllers/default.controller';
+import { createRecord, getAll, updateRecord } from '@controllers/default.controller';
 import { verifyJWT } from '@middlewares/authenticate.mw';
 import { isAllowed } from '@middlewares/isAllowed.mw';
-import { createAlertSchema, getAlertSchema } from '@schemas/alert';
+import { createAlertSchema, getAlertSchema, updateAlertSchema } from '@schemas/alert';
 
 const router = Router();
 
 router.use('/', verifyJWT);
 router.get('/', isAllowed(['alert:view']), getAll(getAlertSchema, 'alert'));
 router.post('/', isAllowed(['alert:create']), createRecord(createAlertSchema, 'alert'));
+router.patch('/', isAllowed(['alert:update']), updateRecord(updateAlertSchema, 'alert'));
 
 export default router;
