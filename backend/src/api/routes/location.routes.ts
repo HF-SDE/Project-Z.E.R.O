40d5c0fd9bca39ffc15e6e69331d8e6138b9f37c
Phiko <phgu03@gmail.com>
2025-01-09 09:29:22 +0100

    ✨ feat(Location): Enhance location routes with create functionality and refined type handling

--- backend/src/api/routes/location.routes.ts
+++ backend/src/api/routes/location.routes.ts
@@ -1,17 +1,24 @@
 import { Router } from 'express';
 
-import { getAll } from '@controllers/default.controller';
+import { LocationWithOutCreateAtAndUpdatedAt } from '@api-types/location.types';
+import { createRecord, getAll } from '@controllers/default.controller';
 import { isAllowed } from '@middlewares/isAllowed.mw';
-import { Location } from '@prisma/client';
-import { getLocationSchema } from '@schemas/location';
+import { createLocationSchema, getLocationSchema } from '@schemas/location';
 
 const router = Router();
 
 router.get(
   '/',
-  isAllowed(['location:view']),
-  getAll<unknown, Location>(getLocationSchema, 'location'),
+  getAll<
+    LocationWithOutCreateAtAndUpdatedAt,
+    LocationWithOutCreateAtAndUpdatedAt
+  >(getLocationSchema, 'location'),
 );
 
+router.post(
+  '/',
+  isAllowed(['location:create']),
+  createRecord(createLocationSchema, 'location'),
+);
 
 export default router;
