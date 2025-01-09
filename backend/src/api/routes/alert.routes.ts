91d2f17ade87077355eb6d44271e69bd30b875e2
Phiko <phgu03@gmail.com>
2025-01-09 13:15:22 +0100

    ✨ feat(routes): Add DELETE route for removing alerts

--- backend/src/api/routes/alert.routes.ts
+++ backend/src/api/routes/alert.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createRecord, getAll, updateRecord } from '@controllers/default.controller';
+import { createRecord, deleteRecord, getAll, updateRecord } from '@controllers/default.controller';
 import { verifyJWT } from '@middlewares/authenticate.mw';
 import { isAllowed } from '@middlewares/isAllowed.mw';
 import { createAlertSchema, getAlertSchema, updateAlertSchema } from '@schemas/alert';
@@ -11,5 +11,6 @@ router.use('/', verifyJWT);
 router.get('/', isAllowed(['alert:view']), getAll(getAlertSchema, 'alert'));
 router.post('/', isAllowed(['alert:create']), createRecord(createAlertSchema, 'alert'));
 router.patch('/', isAllowed(['alert:update']), updateRecord(updateAlertSchema, 'alert'));
+router.delete('/', isAllowed(['alert:delete']), deleteRecord('uuid', 'alert'));
 
 export default router;
