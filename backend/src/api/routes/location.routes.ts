0a3c07fbb4369f597a71e3905892fe61d74dba41
Phiko <phgu03@gmail.com>
2025-01-09 09:29:41 +0100

    ✨ feat(Location): Add JWT verification middleware to location routes

--- backend/src/api/routes/location.routes.ts
+++ backend/src/api/routes/location.routes.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 
 import { LocationWithOutCreateAtAndUpdatedAt } from '@api-types/location.types';
 import { createRecord, getAll } from '@controllers/default.controller';
+import { verifyJWT } from '@middlewares/authenticate.mw';
 import { isAllowed } from '@middlewares/isAllowed.mw';
 import { createLocationSchema, getLocationSchema } from '@schemas/location';
 
 const router = Router();
 
+router.use('/', verifyJWT);
 router.get(
   '/',
   getAll<
