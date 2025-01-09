421a2430004b6f7d2355538cbe95f7ef136a4a83
Phiko <phgu03@gmail.com>
2025-01-09 10:06:55 +0100

    ✨ feat(Location): Add update route for location

--- backend/src/api/routes/location.routes.ts
+++ backend/src/api/routes/location.routes.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 
 import { LocationWithOutCreateAtAndUpdatedAt } from '@api-types/location.types';
-import { createRecord, getAll } from '@controllers/default.controller';
+import {
+  createRecord,
+  getAll,
+  updateRecord,
+} from '@controllers/default.controller';
 import { verifyJWT } from '@middlewares/authenticate.mw';
 import { isAllowed } from '@middlewares/isAllowed.mw';
-import { createLocationSchema, getLocationSchema } from '@schemas/location';
+import { createLocationSchema, getLocationSchema, updateLocationSchema } from '@schemas/location';
 
 const router = Router();
 
@@ -22,5 +26,7 @@ router.post(
   isAllowed(['location:create']),
   createRecord(createLocationSchema, 'location'),
 );
+
+router.patch('/', isAllowed(['location:update']), updateRecord(updateLocationSchema, 'location'));
 
 export default router;
