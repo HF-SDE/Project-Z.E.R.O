ba390419727942cc4dc34a7fead6cc8df40bd6bd
Phiko <phgu03@gmail.com>
2025-01-09 10:06:13 +0100

    ✨ feat(Location): Update location schema to use UuidSchema and add updateLocationSchema for batch updates

--- backend/src/schemas/location.ts
+++ backend/src/schemas/location.ts
@@ -1,10 +1,16 @@
 import Joi from "joi";
+import { UuidSchema } from "./general";
 
 export const getLocationSchema = Joi.object({
-    id: Joi.string().guid({version: 'uuidv4', separator: '-'}),
+    id: UuidSchema,
     name: Joi.string().min(3).max(255),
-});
+}).rename('id', 'uuid', { alias: true, ignoreUndefined: true });
 
 export const createLocationSchema = Joi.object({
     name: Joi.string().min(3).max(255).required(),
 });
+
+export const updateLocationSchema = Joi.array().items(Joi.object({
+    uuid: UuidSchema.required(),
+    name: Joi.string().min(3).max(255).required(),
+})).required();
