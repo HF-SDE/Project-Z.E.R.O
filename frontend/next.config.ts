061b2d2db9c1a140679b66edf66421dadbbc2961
Phiko <phgu03@gmail.com>
2025-01-22 11:30:05 +0100

    ✨ feat(config): Refactor next.config.ts to use env for API URLs and improve error logging

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,22 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
-  serverRuntimeConfig: {
-    URL: process.env.SERVER_API_URL ?? "http://localhost:3001/api/"
+  env: {
+    URL: process.env.SERVER_API_URL ?? "http://localhost:3001/api/",
   },
-  publicRuntimeConfig: {
-    URL: process.env.CLIENT_API_URL ?? "http://localhost:3001/api/"
-  }
 };
 
 console.log("SERVER URL", process.env.SERVER_API_URL);
 console.log("CLIENT URL", process.env.CLIENT_API_URL);
 
-if(!process.env.SERVER_API_URL) {
-  console.error("Missing SERVER_API_URL from environment variables")
+if (!process.env.SERVER_API_URL) {
+  console.error("Missing SERVER_API_URL from environment variables");
 }
-if(!process.env.CLIENT_API_URL) {
-  console.error("Missing CLIENT_API_URL from environment variables")
+if (!process.env.CLIENT_API_URL) {
+  console.error("Missing CLIENT_API_URL from environment variables");
 }
 
 export default nextConfig;
