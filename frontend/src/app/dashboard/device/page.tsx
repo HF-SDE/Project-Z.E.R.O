dfd2d76cc6b829de99fa4fb2ad4525b1b8e4ad8f
Phiko <phgu03@gmail.com>
2025-01-24 09:24:21 +0100

    ✨ feat(device): Update Device component to display device names and descriptions

--- frontend/src/app/dashboard/device/page.tsx
+++ frontend/src/app/dashboard/device/page.tsx
@@ -3,11 +3,18 @@ import { backendCall } from "core/api/backendAxios";
 import { useEffect } from "react";
 
 export default async function Device() {
-  const res = await backendCall("device", "GET");
+  const data = await backendCall("device", "GET");
 
   return (
     <main>
-      <Card className="p-4"></Card>
+      {data.data?.map((device) => {
+        return (
+          <Card key={device.id} className="p-4">
+            <h2>{device.name}</h2>
+            <p>{device.description}</p>
+          </Card>
+        );
+      })}
     </main>
   );
 }
