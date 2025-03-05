c04ec000c83632415f97cb70868ff8716097a149
Phiko <phgu03@gmail.com>
2025-03-05 14:27:30 +0100

    🐳 chore: Remove obsolete MQTT 5 Dockerfile and entrypoint script; add new entrypoint script with user permissions

---
+++ docker/mqtt5-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/ash
+set -e
+
+
+mosquitto_passwd -c -b /mosquitto/config/pwfile iot1 12345678
+mosquitto_passwd -b /mosquitto/config/pwfile iot2 12345678
+mosquitto_passwd -b /mosquitto/config/pwfile iot3 12345678
+mosquitto_passwd -b /mosquitto/config/pwfile iot4 12345678
+# Set permissions
+user="$(id -u)"
+if [ "$user" = '0' ]; then
+	[ -d "/mosquitto" ] && chown -R mosquitto:mosquitto /mosquitto || true
+fi
+exec "$@"
