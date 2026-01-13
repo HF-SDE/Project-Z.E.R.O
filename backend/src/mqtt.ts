0a77b394e6b7fd1c5fa5af3961ac8518ab2a7c9f
Nima Badiei <nimabadiei@gmail.com>
2026-01-13 10:13:21 +0100

    Startup mqtt code

--- backend/src/mqtt.ts
+++ backend/src/mqtt.ts
@@ -4,6 +4,9 @@ import config from '@config';
 
 const client = mqtt.connect(config.MQTT_BROKER);
 
-client.on('connect', () => {
-  console.log('Connected');
+client.on('connect', () => console.log('Connected'));
+client.on('disconnect', () => console.log('Disconnected'));
+
+client.subscribe('clients/+/+/value').on('message', (topic, message) => {
+  console.log(`Topic: ${topic} Message: ${message.toString()}`);
 });
